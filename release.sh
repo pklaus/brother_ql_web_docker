04c056cbefe3bad345f07aa470569bda7306561a
Philipp Klaus <klaus@physik.uni-frankfurt.de>
2018-01-24 19:28:02 +0100

    upgraded release.sh: build all RPi1/2/3 in one go

--- release.sh
+++ release.sh
@@ -12,7 +12,27 @@ SHORT_COMMIT=$(echo $COMMIT | cut -c 1-7)
 
 echo "Latest commit in the repository: $SHORT_COMMIT ($COMMIT)"
 
-for folder in resin-raspberry-pi-alpine-3.6 alpine-3.7
+cd resin-raspberry-pi-alpine-3.6 
+  # RPi3
+  sed -i 's|FROM resin/raspberry-pi-alpine:3.6|#FROM resin/raspberry-pi-alpine:3.6|' Dockerfile
+  sed -i 's|#FROM resin/raspberrypi3-alpine:3.6|FROM resin/raspberrypi3-alpine:3.6|' Dockerfile
+  TAG=${USER}/${REPOSITORY}:resin-raspberrypi3-alpine-3.6_${SHORT_COMMIT}
+  docker build -t ${TAG} --squash .
+  docker push ${TAG}
+  sed -i 's|#FROM resin/raspberry-pi-alpine:3.6|FROM resin/raspberry-pi-alpine:3.6|' Dockerfile
+  sed -i 's|FROM resin/raspberrypi3-alpine:3.6|#FROM resin/raspberrypi3-alpine:3.6|' Dockerfile
+  # RPi2 (base image identical to RPi3!)
+  IMAGE=$(docker images -q ${TAG})
+  TAG=${USER}/${REPOSITORY}:resin-raspberry-pi2-alpine-3.6_${SHORT_COMMIT}
+  docker tag ${IMAGE} ${TAG}
+  docker push ${TAG}
+  # RPi1 / Zero / Zero W
+  TAG=${USER}/${REPOSITORY}:resin-raspberry-pi-alpine-3.6_${SHORT_COMMIT}
+  docker build -t ${TAG} --squash .
+  docker push ${TAG}
+cd -
+
+for folder in alpine-3.7
 do
   cd ${folder}
   TAG=${USER}/${REPOSITORY}:${folder}_${SHORT_COMMIT}
@@ -23,5 +43,6 @@ done
 
 # Tag the last built image "latest". It should be the default one. 
 IMAGE=$(docker images -q ${TAG})
-docker tag ${IMAGE} ${USER}/${REPOSITORY}:latest
-docker push ${USER}/${REPOSITORY}:latest
+TAG=${USER}/${REPOSITORY}:latest
+docker tag ${IMAGE} ${TAG}
+docker push ${TAG}
